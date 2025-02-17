5c43e0692c7416e36569f5d2abe86a32b4efbcd5
elliottzheng <admin@hypercube.top>
2025-02-17 12:37:31 +0800

    Update setup.py with package metadata and version bump to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@
 from setuptools import setup
 
 setup(name='ppt2fig',
-      version='1.0.0',
+      version='1.0.1',
+      description='将当前PPT快速导出为PDF并裁剪白边',
+      long_description=open('README.md', encoding='utf-8').read(),
+      long_description_content_type='text/markdown',
+      author='Elliot Zheng',
+      author_email='admin@hypercube.top',
+      url='https://github.com/elliottzheng/ppt2fig',
       packages=['ppt2fig'],
       entry_points={
            'console_scripts': [
@@ -13,5 +19,11 @@ setup(name='ppt2fig',
       install_requires=[
           'comtypes',
           'pdfCropMargins',
-      ]
+      ],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+      ],
+      python_requires='>=3.6'
     )
